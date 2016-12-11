6b9722658f82c7792420d1a9f679a3c08e2d08f9
jiuchabaikaishui <781849568@qq.com>
2016-12-12 02:57:08 +0800

    完成数字读作
    完成数字读作

--- Practice06_iOS/ViewController.m
+++ Practice06_iOS/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -23,6 +24,7 @@
     [super viewDidLoad];
     
     [self.textField becomeFirstResponder];
+    self.label.numberOfLines = 0;
     self.textView.editable = NO;
 }
 
@@ -150,97 +152,196 @@
             }
             else
             {
-                NSString *yuanStr = @"";
-                NSString *jiaoStr = @"";
-                NSString *fenStr = @"";
+                if (((NSString *)[arr firstObject]).length > 16) {
+                    returnModel.code = @"0003";
+                    returnModel.result = @"";
+                    returnModel.message = @"数字过长！";
+                }
                 
                 //计算整数部分即（元）
-                yuanStr = [self calculationYuan:[arr firstObject]];
+                NSString *wholeStr = @"";
+                if (arr.count > 0) {
+                    wholeStr = [self calculationWhole:[arr firstObject]];
+                }
+                //计算小数部分即（元）
+                NSString *fractionalStr = @"";
+                if (arr.count > 1) {
+                    fractionalStr  = [self calculationFractional:arr[1]];
+                }
                 
-                
-                
-                returnModel.result = [NSString stringWithFormat:@"%@%@%@", yuanStr, jiaoStr, fenStr];
+                if ([amount rangeOfString:@"."].length > 0 && [wholeStr isEqualToString:[self calculationWhole:@"0"]]) {
+                    returnModel.result = [NSString stringWithFormat:@"%@", fractionalStr];
+                }
+                else
+                {
+                    returnModel.result = [NSString stringWithFormat:@"%@%@", wholeStr, fractionalStr];
+                }
             }
         }
     }
     
     return returnModel;
 }
+
+/**
+ *  计算小数部分即（角、分）
+ */
+- (NSString *)calculationFractional:(NSString *)amount
+{
+    NSString *str = @"";
+    NSString *subStr;
+    int value;
+    //计算角
+    if (amount.length > 0) {
+        subStr = [amount substringToIndex:1];
+        value = [subStr intValue];
+        if (value > 0) {
+            str = [NSString stringWithFormat:@"%@角", NumberArray[value]];
+        }
+    }
+    //计算分
+    if (amount.length > 1) {
+        subStr = [amount substringWithRange:NSMakeRange(1, 1)];
+        value = [subStr intValue];
+        if (value > 0) {
+            str = [NSString stringWithFormat:@"%@%@分", str, NumberArray[value]];
+        }
+    }
+    
+    return str;
+}
 /**
  *  计算整数部分即（元）
  */
-- (NSString *)calculationYuan:(NSString *)amount
+- (NSString *)calculationWhole:(NSString *)amount
 {
     NSString *result = @"圆";
-    if ([amount integerValue] > 0) {
-        //把字符串从右端开始按照4个字符截断为数字
-        NSMutableArray *mArr = [NSMutableArray arrayWithCapacity:1];
-        NSString *subAmount;
-        while (amount.length > 4) {
-            subAmount = [amount substringFromIndex:amount.length - 4];
-            amount = [amount substringToIndex:amount.length - 4];
-            //去掉头部的0
-            [mArr insertObject:subAmount atIndex:0];
-        }
-        [mArr addObject:amount];
-        
+    //把字符串从右端开始按照4个字符截断为数字
+    NSMutableArray *mArr = [NSMutableArray arrayWithCapacity:1];
+    NSString *subAmount;
+    while (amount.length > 4) {
+        subAmount = [amount substringFromIndex:amount.length - 4];
+        amount = [amount substringToIndex:amount.length - 4];
+        //去掉头部的0
+        [mArr addObject:subAmount];
+    }
+    [mArr addObject:amount];
+    
+    NSString *totalStr = @"";
+    BOOL zero = YES;
+    int count = 0;
+    for (NSString *valueStr in mArr) {
+        int value = [valueStr intValue];
         NSString *str = @"";
-        BOOL noZero;
-        for (NSString *valueStr in mArr) {
-            //去掉字符数组前面全为0的元素
-            if ([valueStr intValue] != 0 || noZero) {
-                int value = [valueStr intValue];
-                int index = 0;
-                switch (index%4) {
-                    case 1:
-                        if (value%10 != 0) {
-                            str = [NSString stringWithFormat:@"%@拾%@", NumberArray[value%10], str];
-                        }
-                        else
-                        {
-                            if (value/10%10 != 0) {
-                                str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
-                            }
-                        }
-                        break;
-                    case 2:
-                        if (value%10 != 0) {
-                            str = [NSString stringWithFormat:@"%@佰%@", NumberArray[value%10], str];
-                        }
-                        else
-                        {
-                            if (value/10%10 != 0) {
-                                str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
-                            }
-                        }
-                        break;
-                    case 3:
-                        if (value%10 != 0) {
-                            str = [NSString stringWithFormat:@"%@仟%@", NumberArray[value%10], str];
-                        }
-                        else
-                        {
-                            str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
-                        }
-                        break;
-                        
-                    default:
-                        break;
+        //去掉字符数组前面全为0的元素
+        if ([valueStr intValue] > 0) {
+            zero = NO;
+            str = [self readFourBitNumber:valueStr];
+        }
+        
+        NSLog(@"%@", str);
+        value = [valueStr intValue];
+        switch (count) {
+            case 1:
+                if (value > 0) {
+                    str = [NSString stringWithFormat:@"%@万", str];
                 }
-                
-                if (value < 10000 && index != 0) {
-                    if ([mArr[index - 1] intValue] > 0) {
-                        str = [NSString stringWithFormat:@"%@%@", [NumberArray firstObject], str];
-                    }
+                break;
+            case 2:
+                str = [NSString stringWithFormat:@"%@亿", str];
+                break;
+            case 3:
+                if (value > 0) {
+                    str = [NSString stringWithFormat:@"%@万", str];
                 }
+                break;
                 
-                index++;
+            default:
+                break;
+        }
+        
+        if (value < 1000 && mArr.count > count + 1) {
+            BOOL noZero = NO;
+            NSString *theStr;
+            for (int i = 1; i < mArr.count; i++) {
+                theStr = mArr[i];
+                if ([theStr intValue] > 0) {
+                    noZero = YES;
+                    break;
+                }
+            }
+            if (noZero && value > 0) {
+                str = [NSString stringWithFormat:@"%@%@", [NumberArray firstObject], str];
             }
         }
         
+        totalStr = [NSString stringWithFormat:@"%@%@", str, totalStr];
+        if (count == mArr.count - 1 && zero) {
+            totalStr = [NSString stringWithFormat:@"%@%@", [NumberArray firstObject], totalStr];
+        }
+        
+        count++;
     }
     
-    return result;
+    return [NSString stringWithFormat:@"%@%@", totalStr, result];
+}
+/**
+ *  大写读4位整数
+ */
+- (NSString *)readFourBitNumber:(NSString *)amount
+{
+    int index = 0, value = [amount intValue];
+    NSString *str = @"";
+    while (value) {
+        switch (index%4) {
+            case 0:
+                if (value%10 != 0) {
+                    str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
+                }
+                break;
+            case 1:
+                if (value%10 != 0) {
+                    str = [NSString stringWithFormat:@"%@拾%@", NumberArray[value%10], str];
+                }
+                else
+                {
+                    NSString *theStr = [amount substringFromIndex:amount.length - index%4];
+                    if (value/10%10 != 0 && ([theStr intValue] > 0)) {
+                        str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
+                    }
+                }
+                break;
+            case 2:
+                if (value%10 != 0) {
+                    str = [NSString stringWithFormat:@"%@佰%@", NumberArray[value%10], str];
+                }
+                else
+                {
+                    NSString *theStr = [amount substringFromIndex:amount.length - index%4];
+                    if (value/10%10 != 0 && ([theStr intValue] > 0)) {
+                        str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
+                    }
+                }
+                break;
+            case 3:
+                if (value%10 != 0) {
+                    str = [NSString stringWithFormat:@"%@仟%@", NumberArray[value%10], str];
+                }
+                else
+                {
+                    str = [NSString stringWithFormat:@"%@%@", NumberArray[value%10], str];
+                }
+                break;
+                
+            default:
+                break;
+        }
+        
+        value /= 10;
+        index++;
+    }
+    
+    return str;
 }
 /**
  *  字符串判空
